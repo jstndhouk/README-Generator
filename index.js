8cc84304f8c5a9903f1c4322f306168125ef4f92
Justin Houk <jstndhouk@gmail.com>
2021-10-22 18:46:26 -0400

    badges work, no errors

--- index.js
+++ index.js
@@ -47,56 +47,69 @@ const questions = [{
         message: 'Provide your email address',
         name: 'email',
     }
-];
+]
 
-// TODO: Create a function to write README file
 inquirer
     .prompt(questions)
     .then((response) =>
-        fs.writeFile('README.md', generateMD(response),err => err ? console.error(err) : console.log('Success!'))
+        fs.writeFile('README.md', generateMD(response) ,err => err ? console.error(err) : console.log('Success!'))
     )
 
-if(technologies.includes('html'))
-   var htmlBadge=`(https://img.shields.io/badge/html5-%23E34F26.svg?style=for-the-badge&logo=html5&logoColor=white)`
-else if(technologies.toLowerCase().includes('css'))
-   var cssBadge=`![CSS3](https://img.shields.io/badge/css3-%231572B6.svg?style=for-the-badge&logo=css3&logoColor=white)`
-else if (technologies.toLowerCase().includes('javaScript'))
-   var jsBadge=`![JavaScript](https://img.shields.io/badge/javascript-%23323330.svg?style=for-the-badge&logo=javascript&logoColor=%23F7DF1E)`
-else if (technologies.toLowerCase().includes('jquery'))
-   var jqBadge=`![jQuery](https://img.shields.io/badge/jquery-%230769AD.svg?style=for-the-badge&logo=jquery&logoColor=white)`
-else if (technologies.toLowerCase().includes('Node.js'))
-    var nodeBadge=`![jQuery](https://img.shields.io/badge/jquery-%230769AD.svg?style=for-the-badge&logo=jquery&logoColor=white)`
-else 
-    var noBadge=`No valid language was chosen! :sob:`
-     //I want to make the promise fail if we get here in the conditional statement
+const generateMD=({title, description, installation, usage, technologies, contribution, tests, username, email}) =>{
+  
+if(technologies.includes('html')){
+    var htmlBadge=`![HTML5](https://img.shields.io/badge/html5-%23E34F26.svg?style=for-the-badge&logo=html5&logoColor=white)`
+    console.log('html success!')}
+    else
+    var htmlBadge='';
+if(technologies.toLowerCase().includes('css')){
+    var cssBadge=`![CSS3](https://img.shields.io/badge/css3-%231572B6.svg?style=for-the-badge&logo=css3&logoColor=white)`
+    console.log('css success!')}
+    else
+    var cssBadge='';
+if (technologies.toLowerCase().includes('javascript')){
+    var jsBadge=`![JavaScript](https://img.shields.io/badge/javascript-%23323330.svg?style=for-the-badge&logo=javascript&logoColor=%23F7DF1E)`
+    console.log('javascript success!')}
+    else
+    var jsBadge='';
+if (technologies.toLowerCase().includes('jquery')){
+    var jqBadge=`![jQuery](https://img.shields.io/badge/jquery-%230769AD.svg?style=for-the-badge&logo=jquery&logoColor=white)`
+    console.log('jquery success!')}
+    else
+    var jqBadge='';
+if (technologies.toLowerCase().includes('node.js')){
+    var nodeBadge=`![NodeJS](https://img.shields.io/badge/node.js-6DA55F?style=for-the-badge&logo=node.js&logoColor=white)`
+    console.log('node success!')}
+    else
+    var nodeBadge='';
 
-const badgeString=`${htmlBadge} ${cssBadge} ${jsBadge}  ${jqBadge}  ${nodeBadge}  ${noBadge}`
-   
-
-const generateMD=({title, description, installation, usage, contribution, tests, username, email}, badgeString) =>
+const badgeString=`${htmlBadge} ${cssBadge} ${jsBadge}  ${jqBadge}  ${nodeBadge}`
+console.log(badgeString)
+const readmeString=
 `# **${title}**
 ![Image output](https://img.shields.io/github/issues/${username}/${title}?style=for-the-badge)
-
-#Technologies Used: 
+# Technologies Used:
 ${badgeString}
-
-
-# Description: ${description}
-         
-         
-# Installation Instructions: \n${installation}
-         
-         
+\n
+[!['s top languages](https://github-readme-stats.vercel.app/api/top-langs/?username=${username}&theme=blue-green)](https://github.com/${username}/${title})
+# Description:\n ${description}
+\n
+\n       
+# Installation: \n${installation}
+\n
+\n        
 # Usage: \n${usage}
-         
-         
-# Contributions: \n${contribution}
-         
-         
+\n
+\n          
+# Contributing: \n${contribution}
+\n
+\n         
 # Tests: \n${tests}
-         
-         
-# Github: ${username}
-         
-         
-# Email:${email}`
+\n
+\n          
+# Questions: \n 
+Github: https://github.com/${username}
+\n\n      
+If you would to reach out to me personally, feel free to email me: ${email}`
+return readmeString;
+}
