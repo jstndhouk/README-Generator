16004cc82f7af28948a446b361ea6de7b9a0abd7
Justin Houk <jstndhouk@gmail.com>
2021-10-23 18:47:57 -0400

    done

--- index.js
+++ index.js
@@ -2,7 +2,8 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
 // TODO: Create an array of questions for user input
-const questions = [{
+const questions = [
+    {
         type: 'input',
         message: 'What is the name of your project?',
         name: 'title',
@@ -30,7 +31,13 @@ const questions = [{
     {
         type: 'input',
         message: 'Provide any test instructions.',
-        name: 'tests',
+        name: 'tests', 
+    },
+    {
+        type: 'list',
+        message: 'Which license are you using?',
+        name: 'license',
+        choices: ['Apache 2.0', 'Boost', 'BSD', 'None']
     },
     {
         type: 'input',
@@ -49,65 +56,110 @@ const questions = [{
     }
 ]
 
+function renderLicenseBadge(license) 
+{
+    switch(license){
+     case 'Apache 2.0': return `[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)`
+    case 'Boost': return `[![License](https://img.shields.io/badge/License-Boost_1.0-lightblue.svg)](https://www.boost.org/LICENSE_1_0.txt)`
+    case 'BSD': return `[![License](https://img.shields.io/badge/License-BSD_3--Clause-blue.svg)](https://opensource.org/licenses/BSD-3-Clause)`
+    case 'None': return ``
+    }
+}
+
+// TODO: Create a function that returns the license link
+// If there is no license, return an empty string
+function renderLicenseLink(license) 
+{
+    if (license=='None')
+        return ''
+    else
+        return `* [License](#license)`
+}
+
+// TODO: Create a function that returns the license section of README
+// If there is no license, return an empty string
+function renderLicenseSection(license) 
+{
+    if (license=='None')
+    return ``
+    else
+    {
+    return `## License:\nThe license that was chosen for this project is ${license}
+    \n 
+    \n
+    `
+    }
+}
+
 inquirer
     .prompt(questions)
     .then((response) =>
         fs.writeFile('README.md', generateMD(response) ,err => err ? console.error(err) : console.log('Success!'))
     )
 
-const generateMD=({title, description, installation, usage, technologies, contribution, tests, username, email}) =>{
-  
-if(technologies.includes('html')){
-    var htmlBadge=`![HTML5](https://img.shields.io/badge/html5-%23E34F26.svg?style=for-the-badge&logo=html5&logoColor=white)`
-    console.log('html success!')}
-    else
-    var htmlBadge='';
-if(technologies.toLowerCase().includes('css')){
-    var cssBadge=`![CSS3](https://img.shields.io/badge/css3-%231572B6.svg?style=for-the-badge&logo=css3&logoColor=white)`
-    console.log('css success!')}
-    else
-    var cssBadge='';
-if (technologies.toLowerCase().includes('javascript')){
-    var jsBadge=`![JavaScript](https://img.shields.io/badge/javascript-%23323330.svg?style=for-the-badge&logo=javascript&logoColor=%23F7DF1E)`
-    console.log('javascript success!')}
-    else
-    var jsBadge='';
-if (technologies.toLowerCase().includes('jquery')){
-    var jqBadge=`![jQuery](https://img.shields.io/badge/jquery-%230769AD.svg?style=for-the-badge&logo=jquery&logoColor=white)`
-    console.log('jquery success!')}
-    else
-    var jqBadge='';
-if (technologies.toLowerCase().includes('node.js')){
-    var nodeBadge=`![NodeJS](https://img.shields.io/badge/node.js-6DA55F?style=for-the-badge&logo=node.js&logoColor=white)`
-    console.log('node success!')}
-    else
-    var nodeBadge='';
 
-const badgeString=`${htmlBadge} ${cssBadge} ${jsBadge}  ${jqBadge}  ${nodeBadge}`
-console.log(badgeString)
+    const generateMD=({title, description, installation, usage, technologies, contribution, tests, username, email, license}) =>
+{
+  
+    if(technologies.includes('html'))
+        var htmlBadge=`![HTML5](https://img.shields.io/badge/html5-%23E34F26.svg?style=for-the-badge&logo=html5&logoColor=white)`
+        else
+        var htmlBadge='';
+    if(technologies.toLowerCase().includes('css'))
+        var cssBadge=`![CSS3](https://img.shields.io/badge/css3-%231572B6.svg?style=for-the-badge&logo=css3&logoColor=white)`
+        else
+        var cssBadge='';
+    if (technologies.toLowerCase().includes('javascript'))
+        var jsBadge=`![JavaScript](https://img.shields.io/badge/javascript-%23323330.svg?style=for-the-badge&logo=javascript&logoColor=%23F7DF1E)`
+        else
+        var jsBadge='';
+    if (technologies.toLowerCase().includes('jquery'))
+        var jqBadge=`![jQuery](https://img.shields.io/badge/jquery-%230769AD.svg?style=for-the-badge&logo=jquery&logoColor=white)`
+        else
+        var jqBadge='';
+    if (technologies.toLowerCase().includes('node.js'))
+        var nodeBadge=`![NodeJS](https://img.shields.io/badge/node.js-6DA55F?style=for-the-badge&logo=node.js&logoColor=white)`
+    
+        else
+        var nodeBadge='';
+
+    const badgeString=`${htmlBadge} ${cssBadge} ${jsBadge}  ${jqBadge}  ${nodeBadge}`
 const readmeString=
 `# **${title}**
+${renderLicenseBadge(license)}
+## Table of Contents
+* [Description](#description)
+* [Installation](#installation)
+* [Usage](#usage)
+* [Contributing](#contributing)
+* [Tests](#tests)
+${renderLicenseLink(license)}
+* [Questions](#questions)
+
 ![Image output](https://img.shields.io/github/issues/${username}/${title}?style=for-the-badge)
 # Technologies Used:
 ${badgeString}
 \n
 [!['s top languages](https://github-readme-stats.vercel.app/api/top-langs/?username=${username}&theme=blue-green)](https://github.com/${username}/${title})
-# Description:\n ${description}
+## Description:\n ${description}
 \n
 \n       
-# Installation: \n${installation}
+## Installation: \n${installation}
 \n
 \n        
-# Usage: \n${usage}
+## Usage: \n${usage}
 \n
 \n          
-# Contributing: \n${contribution}
+## Contributing: \n${contribution}
 \n
-\n         
-# Tests: \n${tests}
+\n    
+${renderLicenseSection(license)}
 \n
 \n          
-# Questions: \n 
+## Tests: \n${tests}
+\n
+\n          
+## Questions: \n 
 Github: https://github.com/${username}
 \n\n      
 If you would to reach out to me personally, feel free to email me: ${email}`
